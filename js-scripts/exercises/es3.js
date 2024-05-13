396a705d0a7ef994ae79eb4e9cb10c83ba59bd92
Muhammad Shariq Abdullah <145561688+ShariqAwaan@users.noreply.github.com>
2024-05-13 17:59:20 +0500

    Updated Code

---
+++ js-scripts/exercises/es3.js
@@ -0,0 +1,49 @@
+
+
+// Write a JavaScript program that iterates integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers multiples of both three and five print "FizzBuzz".
+
+// Write a JavaScript program to find the Armstrong numbers of 3 digits.
+// Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
+// Write a JavaScript program to sum 3 and 5 multiples under 1000.
+
+//  Calculate the sum of all the numbers in the following array
+// var numbersArray = [1,13,22,123,49]
+
+// Write a program that determines whether a given number is positive or negative.
+
+// Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+
+// Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0
+
+// Get the sum of two arrays…actually the sum of all their elements.
+// P.S. Each array includes only integer numbers. Output is a number too.
+// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+// // Example output: 
+// // 276 + 351 = 627
+
+// Using a for loop output the elements in reverse order
+
+
+//Q1
+// Write a JavaScript for loop that iterates from 0 to 15. For each iteration, it checks if the current number is odd or even, and displays a message on the screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+
+
+let btnQ1 = document.getElementById("btnQ1");
+let answerQ1 = document.getElementById("pQ1");
+
+
+function solutionQ1(){
+  
+  for (let i=0 ; i<= 15 ; i++){
+    i++
+  }
+}
+
+
+
